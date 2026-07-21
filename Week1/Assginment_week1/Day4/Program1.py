a016460526f5b6150d228354462fd66173d91ded
Niraj-Ramnani <neerajramnani800@gmail.com>
2026-07-21 17:11:45 +0530

    updated weekly assignment program

--- Week1/Assginment_week1/Day4/Program1.py
+++ Week1/Assginment_week1/Day4/Program1.py
@@ -1,24 +1,23 @@
 1. # Write a program to add two matrices.
 import numpy as np
 
+def getdata():
+    matrix = []
+    for i in range(rows):
+        row = list(map(int,input(f"Enter row {i +1 } ").split()))
+        if len(row) != cols:
+            raise ValueError(f"row must contain {cols} elements ")
+        matrix.append(row)
+    return matrix
+
 try:
     rows = int(input("Enter number of rows : "))
     cols = int(input("Enter number of columns : "))
     print("Enter elements for matrix 1 : ")
-    matrix1 = []
-    for i in range(rows):
-        row = list(map(int,input(f"Enter row {i +1 } ").split()))
-        if len(row) != cols:
-            raise ValueError(f"row must contain {cols} elements ")
-        matrix1.append(row)
+    matrix1 = getdata()
 
     print("Enter elements for matrix 2 : ")
-    matrix2 = []
-    for i in range(rows):
-        row = list(map(int,input(f"Enter row {i +1 } ").split()))
-        if len(row) != cols:
-            raise ValueError(f"row must contain {cols} elements ")
-        matrix2.append(row)
+    matrix2 = getdata()
 
     matrix1 = np.array(matrix1)
     matrix2 = np.array(matrix2)
